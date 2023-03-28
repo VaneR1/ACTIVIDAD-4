038908d58652f55538dfa37ae87aabd00fe29911
Vane <vanessarios_11@hotmail.com>
2023-03-28 08:21:12 -0500

    Punto 1

---
+++ OneDrive/Escritorio/ACTIVIDAD 4/punto_1.py
@@ -0,0 +1,7 @@
+Celsius=float(input('Ingrese el numero en Celsius: '))
+fahrenhait=((9/5)*Celsius)+32
+kelvin=Celsius+273.15
+rankine=((Celsius*1.8)+491.67)
+print('La temperatura en fahrenhait es:',fahrenhait)
+print('La temperatura en Kelvin es:',kelvin)
+print('La temperatura en Rankine es: ',rankine)
